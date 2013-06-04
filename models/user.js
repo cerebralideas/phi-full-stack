3cff92bb57e2abf0e38cae6825e729c983a9cd00
Nate Jackman <natejackman@gmail.com>
2013-06-04 15:51:59 -0700

    password encrypting and salting

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose'),
+	crypto = require('crypto'),
 	Promise = mongoose.Promise,
 	userSchema = mongoose.Schema({
 		email: String,
-		password: String
+		hashed_password: String,
+		salt: String
 	});
 
 userSchema.statics.getUser = function(id, callback) {
@@ -22,5 +24,28 @@ userSchema.statics.getUser = function(id, callback) {
 	this.find(id, promise.resolve.bind(promise));
 	return promise;
 };
+
+userSchema.methods = {
+
+	makeSalt: function() {
+		return Math.round((new Date().valueOf() * Math.random())) + ''
+	},
+
+	encryptPassword: function(password) {
+		if (!password) return ''
+		return crypto.createHmac('sha1', this.salt).update(password).digest('hex')
+	}
+};
+
+userSchema.virtual('password')
+	.set(function(password) {
+		this._password = password
+		this.salt = this.makeSalt()
+		this.hashed_password = this.encryptPassword(password)
+	})
+	.get(function() { 
+		return this._password 
+	});
+
 
 module.exports = mongoose.model('User', userSchema); 
